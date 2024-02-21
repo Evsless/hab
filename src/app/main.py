faaa8e8a272c5a9a095499d07dd279429d16c6ee
Yahor Yauseyenka <yahoryauseyenka@gmail.com>
2024-02-21 21:09:54 +0100

    HAB_SW_005: Updated the AD5272 interface implementation
    1) Prepared the R/W methods for communicating with
       the module;
    
    TBD: An additional refactoring might be needed.

--- src/app/main.py
+++ src/app/main.py
@@ -1,18 +1,29 @@
 import board
 import busio
-
 import time
+
 from ads111x.ads1115 import ads1115
 from ads111x import ADS1115_CH
 
-from adafruit_bus_device.i2c_device import I2CDevice
+from ad527x.ad5272 import ad5272
+from ad527x import AD5272_REG
 
 i2c0 = busio.I2C(board.D1, board.D0)
 i2c1 = busio.I2C(board.D3, board.D2)
 
 ads1115_1 = ads1115(0x49, i2c1)
+ad5272_1  = ad5272(0x2c,  i2c1)
+
+wiper_pos = 1
 
 while True:
+    ad5272_1.write_rdac(wiper_pos)
+
     a = ads1115_1.read(ADS1115_CH.CH0.value)
-    print(a[0] << 8 | a[1])
+    digipot = ad5272_1.read(AD5272_REG.RDAC_R.value)
+
+    print("ADC CH0 VALUE: ", a[0] << 8 | a[1])
+    print(f"DIGIPOT VALUE: {digipot}\n")
+
+    wiper_pos += 1
     time.sleep(1)
--- src/lib/ad527x/ad5272.py
+++ src/lib/ad527x/ad5272.py
@@ -1,30 +1,43 @@
-from . import const
+from .      import const
+from .utils import *
+from i2clib.i2c_handler import i2c_handler
 
-class ad5272_controller:
+class ad5272:
     def __init__(self, address, i2c_bus) -> None:
         self._address = const(address)
         self._i2c     = i2c_bus
         self._r_buff  = bytearray(2)
 
-    def write(self, reg, val) -> None:
-        while not self._i2c.try_lock():
+        self.write_cfg(AD5272_CFG_WP_DIS)
+
+    def _write_reg(self, reg, val = 0x00) -> None:
+        with i2c_handler(self._i2c) as i2c_if:
+            i2c_if.writeto(self._address, bytearray([(reg << 2) | ((val >> 8) & 0x03), val & 0xFF]))
+
+    def _read_response(self) -> None:
+        with i2c_handler(self._i2c) as i2c_if:
+            i2c_if.readfrom_into(self._address, self._r_buff)
+
+    def write_rdac(self, val) -> None:
+        if AD5272_CONST_RDAC_MIN <= val <= AD5272_CONST_RDAC_MAX:
+            self._write_reg(AD5272_CMD_WRITE_RDAC, val)
+        else:
             pass
 
-        try:
-            self._i2c.writeto(self._address, bytearray([(reg << 2) | ((val >> 8) & 0x03), val & 0xFF]))
-        finally:
-            self._i2c.unlock()
+    def write_cfg(self, val) -> None:
+        if 0 <= val <= 0x0F:
+            self._write_reg(AD5272_CMD_WRITE_CTRL_REGISTER, val)
+        else:
+            pass
 
     def read(self, reg) -> int:
-        while not self._i2c.try_lock():
+        if reg in (AD5272_CMD_READ_RDAC, \
+                    AD5272_CMD_READ_CTRL_REGISTER, \
+                    AD5272_CMD_READ_50TP_CONTENT, \
+                    AD5272_CMD_READ_50TP_LAST_ADDR):
+            self._write_reg(reg)
+            self._read_response()
+        else:
             pass
 
-        try:
-            self._i2c.writeto(self._address, bytearray([reg << 2, 0x00]))
-            self._i2c.readfrom_into(self._address, self._r_buff)
-            result = ((self._r_buff[0] & 0x03) << 8) | self._r_buff[1]
-
-        finally:
-            self._i2c.unlock()
-
-        return result
+        return ((self._r_buff[0] & 0x03) << 8) | self._r_buff[1]
